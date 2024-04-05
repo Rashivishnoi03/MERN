6ae7d51eb3e7e570d228c25c216bc44bbfe5becd
RASHI VISHNOI <142437270+Rashivishnoi03@users.noreply.github.com>
2024-04-05 16:16:44 +0530

    Add files via upload

---
+++ Day 4/script.js
@@ -0,0 +1,136 @@
+// let a = new String("Hello");    //object type
+// let b = "Hello";    //string type
+
+// if(a==b){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+
+// const obj = {
+//     "name" : "Rashi",
+//     "lastName" : "Vishnoi"
+
+// }
+
+
+
+// obj.name = 24;
+// console.log(obj);
+// document.write(JSON.stringify(obj));
+
+// const arr = [1,2,3,4,5];
+//  arr = [1,2,3,4,5];
+// console.log(arr);
+
+// const arr = [1,2,3,4,5];     //index is called key
+// arr[1]=67;
+// // arr[10]=20;
+// arr.push(30);
+// console.log(arr);
+// console.log(typeof{arr});
+
+// const obj = {
+//     "name" : "Rashi",
+//     "lastName" : "Vishnoi"
+// }
+// const arr = [1,2,3,4,5]; 
+// // console.log(obj.Rashi);
+// console.log(Array.isArray(obj))
+// console.log(Array.isArray(arr))
+
+// function checkIfObject(x){
+//   if(Array.isArray(x)){
+//     console.log("not object");
+//   }
+//   else if(typeof(x)=="object"){
+//     console.log("object");
+//   } 
+//   else{
+//     console.log(" not object");
+//   }
+// }
+// checkIfObject(20);
+
+// checkIfObject({"name": "Rashi"});
+
+
+// const arr = ['name',2];
+// const obj = {
+//     'name ': "rashi", age :20,1:"else",
+// };
+// // for(let i=0;i<4;i++){
+// //     console.log(obj[i]);
+// // }
+
+// for(let i of arr){
+//     console.log(i);
+// }
+
+// for(let i in arr){
+//     console.log(i);
+// }
+// for(let i in arr){
+//     console.log(arr[i]);
+// }
+// for(let i in obj){
+//     console.log(i);
+// }
+
+// window.console.log(window);
+// window.console.log(window.innerHeight);
+// window.console.log(window.innerWidth);
+
+
+// console.log(window.documemt)
+// console.log(window)
+
+// console.log(window.document)
+// console.dir(window.document)
+
+// const res = document.getElementsByTagName('h2');
+// console.log(res);
+
+// const res = document.getElementById('ht1');
+// console.log(res);
+// console.dir(res);
+
+// const res = 
+// document.querySelectorAll('div>h2');
+// console.log(res);
+
+// const res = 
+// document.querySelectorAll("ht1");
+// res['innerHTML']="Hello";
+// res['innerText']="Hello";
+// console.log(res);
+
+// const res = 
+// document.getElementsByTagName("div");
+// // res[0].innerHTML= "hii";
+// // res[0].innerText= "hloo hii";
+// res[0].innerHTML= "<h4>HELLO,world";
+// console.log(res);
+
+// const res =
+// document.getElementsByTagName("p");
+// console.log(res);
+// res[0].setAttribute('class','c1');
+
+
+
+
+const cr = document.createElement("h3");
+cr.innerText = "Dynamic Text"
+// console.log(cr);
+// document.body.appendChild(cr);
+const firstDiv = document.getElementsByTagName("div");
+// firstDiv[0].appendChild(cr);
+firstDiv[1].appendChild(cr);
+firstDiv[0].remove(cr);    
+// firstDiv[0].removeChild(cr);    
+
+
+
